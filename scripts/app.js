87be7dcf20a915d8cdcb23fc9cf2ebcce36299af
Charlie Guan <cguan@blastworksinc.com>
2017-12-21 17:39:06 -0800

    Separate modules into multiple files. Make explorer move right.

--- scripts/app.js
+++ scripts/app.js
@@ -1,66 +1,13 @@
-// Setup APIs
-const Container = PIXI.Container;
-const Sprite = PIXI.Sprite; const Point = PIXI.Point; 
-let renderer, stage;
-let dungeon, explorer, treasure, door;
-function init() {
-    renderer = PIXI.autoDetectRenderer(1024, 768);
-    document.body.appendChild(renderer.view);
+const DH = window.dungeonHunter;
+DH.init();
+DH.loadCharacters();
 
-    stage = new Container();
-    renderer.render(stage);
-
-    scaleToWindow(renderer.view);
-    window.addEventListener("resize", event => {
-        scaleToWindow(renderer.view);
-    });
+function gameLoop() {
+    requestAnimationFrame(gameLoop);
+    DH.characters.explorer.x += 1;
+    DH.renderer.render(DH.stage);
 }
 
-function loadCharacters() {
-    PIXI.loader.add("assets/treasureHunter.json")
-        .on("progress", logProgress)
-        .load(setup);
-
-        function logProgress(loader, resource) {
-            console.log("Process: " + Math.round(loader.progress) + "%");
-            console.log("File loaded: " + resource.name);
-        }
-
-        function setup(loader, resources) {
-            const spritesheet = resources["assets/treasureHunter.json"].textures;
-            dungeon = new Sprite(spritesheet["dungeon.png"]);
-            explorer = new Sprite(spritesheet["explorer.png"]);
-            treasure = new Sprite(spritesheet["treasure.png"]);
-            door = new Sprite(spritesheet["door.png"]);
-
-            const numberOfBlobs = 6,
-            spacing = 48, 
-            xOffset = 150;
-
-            stage.addChild(dungeon);
-            stage.addChild(explorer);
-            stage.addChild(treasure);
-            stage.addChild(door);
-
-            explorer.position = new Point(68, stage.height / 2 - explorer.height / 2);
-            treasure.position = new Point(stage.width - treasure.width - 48, stage.height / 2 - treasure.height / 2);
-            door.position = new Point(32, 0);
-
-            for (let i = 0; i < numberOfBlobs; i++) {
-                const blob = new Sprite(spritesheet["blob.png"]);
-                blob.position = new Point(spacing * i + xOffset, getRandomInt(0, stage.height - blob.height));
-                stage.addChild(blob);
-            }
-            renderer.render(stage);
-
-            function getRandomInt(min, max) {
-                min = Math.ceil(min);
-                max = Math.floor(max);
-                return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-            }
-        }
-}
+gameLoop();
 
 
-init();
-loadCharacters();
